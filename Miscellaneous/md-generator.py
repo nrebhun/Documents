df53f74669e5d5a8da4b6d1ca332528e6b660368
nrebhun <nick@nrebhun.com>
2017-09-18 14:52:06 -0700

    Fix linkification

--- Miscellaneous/md-generator.py
+++ Miscellaneous/md-generator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 import os, argparse, re
 
 header_prefix = "### "
-back_to_top = u'###### [\xe286910a Back to the Top]'
+back_to_top = u'###### [↑ Back to the Top]'
 md_break = "\n\n"
 
 parser = argparse.ArgumentParser()
-parser.add_argument("path_to_file", help="jeeeeeeez... you didn't do it right")
+parser.add_argument("filepath", type=str,
+                    help="Some filepath you'd like to generate documentation snippets for.")
 
 args = parser.parse_args()
 
@@ -16,8 +17,10 @@ def generate_section(file_path = None):
     header = assemble_header(base_name)
     content = "Full filepath: `" + file_path + "`" + md_break + "Add useful info here"
 
-    link = linkify(base_name)
+    link = linkify(file_path, base_name)
 
+    # This needs to change, to process multi-line input.
+    # Most variable assignments will simply need to be converted to array concats
     generated_section = header + md_break + content + md_break + back_to_top + md_break + link
 
     return generated_section
@@ -25,20 +28,18 @@ def generate_section(file_path = None):
 def assemble_header(header_content = None):
     return header_prefix + header_content
 
-def linkify(filename = None):
-    link_text = "[" + filename + "]: "
-    link_location = remove_specials(filename).lower()
-    result = link_text + "#"+ link_location
+def linkify(file_path = None, base_name = None):
+    link_text = "[" + file_path + "]: "
+    link_location = remove_specials(base_name).lower()
+    result = link_text + "#"+ base_name
 
     return result
 
 def remove_specials(input_string = None):
-    return re.sub('[/!@#$~\-\]', '', input_string)
+    return re.sub('[/!@#$~\-]', '', input_string)
 
 def usage():
-    path_to_file = None
-
-    if args.path_to_file:
-        print generate_section(args.path_to_file)
+    if args.filepath:
+        print generate_section(args.filepath)
 
 usage()
